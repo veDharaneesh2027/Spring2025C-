009d763471b8c73188152b125f90319a4f0921f0
vedha <venkatadharaneesh2005@gmail.com>
2025-04-03 14:56:10 -0700

    freestyle1

--- main.cpp
+++ main.cpp
@@ -2,21 +2,12 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-// Problem 1: Power of Two
-int main() {
-    int number;
-    std::cout << "Enter a number: ";
-    cin>>number;
-    if (number > 0) {
-        while
+    int main() {
+        return 0;
     }
-    return 0;
-}
 
-// Problem 3
-int main3() {
 
-}
+
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
